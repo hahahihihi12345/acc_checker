a32411ee0bf96ba76d9e00cac6833b1ca85a8094
hahahihihi12345 <hahahihihi12345@gmail.com>
2024-07-01 23:05:32 +0200

    Update followers.py
    added option of recursive search

--- followers.py
+++ followers.py
@@ -12,11 +12,7 @@ class LinkedList:
 
     def pop(self):
         head = self.head
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = self.head.next
+        self.delete(self.head)
         return head
 
     def append(self,val):
@@ -26,11 +22,32 @@ class LinkedList:
         else:
             self.tail.next = Node(val)
             self.tail = self.tail.next
+    
+    def delete(self, node):
+        if node is self.head:
+            self.head = node.next
+        
+        if node is self.tail:
+            self.tail = node.prev
+        
+        if node.next is not None:
+            node.next.prev = node.prev
+        
+        if node.prev is not None:
+            node.prev.next = node.next
+
+    def join(self, linked):
+        if linked.head is None:
+            return
+        self.append(linked.head)
+        self.tail = linked.tail
+
 
 class Node:
     def __init__(self, val):
         self.value = val
         self.next = None
+        self.prev = None
 
 
 def get_names() -> list[str]:
@@ -75,9 +92,9 @@ def log_in(USERNAME, PASSWORD, driver):
 
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, \
         '/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button'))).click()
-    
 
-def get_followers(username, driver) -> LinkedList:
+
+def get_followers(username, driver, keywords, seen: set[tuple[str, str]]) -> LinkedList:
     followers = LinkedList()
     url = "https://www.instagram.com/" + username + "/following/"
     driver.get(url)
@@ -91,7 +108,6 @@ def get_followers(username, driver) -> LinkedList:
         scroll_down(driver)
         time.sleep(4)
         new_loaded = driver.find_elements(By.XPATH, x)
-        
 
 
     # iterate through followed accounts
@@ -100,8 +116,9 @@ def get_followers(username, driver) -> LinkedList:
         #full name div /div/div/div/div[2]/div/div/span/span
         name = div.find_element(By.XPATH, "./div/div/div/div[2]/div/div/div/div/div/a/div/div/span").text
         fullname = div.find_element(By.XPATH, "./div/div/div/div[2]/div/div/span/span").text
-
-        followers.append((name, fullname))
+        if has_keyword(name, keywords) or has_keyword(fullname, keywords) and (name, fullname) not in seen: 
+            followers.append((name, fullname))
+            seen.add((name, fullname))
 
     return followers
 
@@ -111,7 +128,7 @@ def has_keyword(name: str, keywords: list[str]) -> bool:
     change = {"0":"o", "1":"i", "2":"", "3":"e", "4":"a", "5":"s", "6":"", "7":"t", "8":"", "9":""}
     name = name.lower()
     enc_name = "".join([char for char in name if char.isalpha()])
-    num_enc_name = "".join([change[char] if char.isnumeric() else char for char in name if char.isalnum()])
+    num_enc_name = "".join([change[char] if char in change else char for char in name if char.isalnum()])
     for keyword in keywords:
         if keyword in name or keyword in enc_name or keyword in num_enc_name:
             return True
@@ -119,20 +136,26 @@ def has_keyword(name: str, keywords: list[str]) -> bool:
 
 
 def printout(linked: LinkedList, file):
-    keywords = get_filter()
+    printed = set()
     file.write("scraped followers: \n")
     while linked.head is not None:
         text = linked.pop().value
-        for name in text:
-            if has_keyword(name, keywords):
-                file.write(", ".join(text) + "\n")
-                break
+        if type(text) != tuple or len(text) != 2:
+            print(text)
+            continue
+        if text not in printed:
+            file.write(", ".join(text) + "\n")
+            printed.add(text)
     file.write("\n")
 
 
 def main():
-    usernames_to_check = get_names()
     USERNAME, PASSWORD = get_creds()
+    keywords = get_filter()
+    usernames_to_check = get_names()
+    seen = set()
+    # depth of recursive search
+    depth = 0
 
     # open browser & log in Instagram
     driver = webdriver.Edge()
@@ -143,10 +166,26 @@ def main():
 
     with open("followers.txt", "w", encoding = "utf-8") as res_file:
         for username in usernames_to_check:
-            #try:
+            new_depth = depth
+            if True:#try:
                 print("trying username: " + username)
-                followers = get_followers(username, driver)
-                res_file.write("Root username: " + username + "\n")
+                followers = get_followers(username, driver, keywords, seen)
+                print("got first followers")
+                curr_node = followers.head
+
+                while new_depth != 0:
+                    print("rec depth: " + str(new_depth))
+                    last_node = followers.tail
+                    while True:
+                        new_followers = get_followers(curr_node.value[0], driver, keywords, seen)
+                        if curr_node is last_node:
+                            break
+                        curr_node = curr_node.next
+
+                    followers.join(new_followers)
+                    new_depth -= 1
+                    curr_node = curr_node.next
+
                 printout(followers, res_file)
                 print("attempt successful")
             #except:
